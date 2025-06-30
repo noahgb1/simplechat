12f2fb99029185f0c87eb86f5b7c349d80a186fc
Paul Lizer <paul.lizer@live.com>
2025-06-30 14:07:54 -0400

    v0.216.003

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -86,7 +86,7 @@ app.config['EXECUTOR_MAX_WORKERS'] = 30
 executor = Executor()
 executor.init_app(app)
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['VERSION'] = '0.216.002'
+app.config['VERSION'] = '0.216.003'
 Session(app)
 
 CLIENTS = {}
